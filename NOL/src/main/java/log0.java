0aa6067f6b098117b669b9f018390aa36f1bba9f
angelgr1303 <126097519+angelgr1303@users.noreply.github.com>
2024-05-16 12:18:45 +0200

    Create log0.java

---
+++ NOL/src/main/java/log0.java
@@ -0,0 +1,64 @@
+
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class log0
+ */
+public class log0 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public log0() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Información relevante
+        String formData = request.getQueryString();
+        String clientInfo = request.getRemoteAddr();
+        LocalDateTime currentDate = LocalDateTime.now();
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+
+        // Construir el mensaje de registro
+        String logMessage = currentDate + formData + " " + clientInfo + " " + uri + " acceso" + method;
+
+        // Registrar el mensaje en el registro del servidor
+        getServletContext().log(logMessage);
+
+        // Enviar la respuesta al cliente
+        response.setContentType("text/plain");
+        response.getWriter().println(logMessage);
+    }
+
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Información relevante
+        String formData = request.getQueryString();
+        String clientInfo = request.getRemoteAddr();
+        LocalDateTime currentDate = LocalDateTime.now();
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+
+        // Construir el mensaje de registro
+        String logMessage = currentDate + formData + " " + clientInfo + " " + uri + " acceso" + method;
+
+        // Registrar el mensaje en el registro del servidor
+        getServletContext().log(logMessage);
+
+        // Enviar la respuesta al cliente
+        response.setContentType("text/plain");
+        response.getWriter().println(logMessage);
+    }
+}
